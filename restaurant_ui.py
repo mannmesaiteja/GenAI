e6976a6e1e181b160e7fc459e3bf62963fcc33b3
Mannem Saiteja <mannemsaiteja76@gmail.com>
2025-11-15 00:28:06 +0530

    Initial commit

--- restaurant_ui.py
+++ restaurant_ui.py
@@ -1,1 +1,22 @@
+import streamlit as st
 
+st.title("Restaurant Name Generator")
+
+cuisine = st.sidebar.selectbox("pick a cuisine", ("Indian", "American", "Mexican", "Italian")) # store the selected item in a variable
+
+
+def generate_restaurant_and_items(cu):
+    return (
+        {
+            "restaurant_name": "Venkateswara_Restaurant",
+            "menu_items": "Biriyani, Fried Chicken, Mutton Curry, Full meals"
+        }
+    )
+
+if cuisine:
+    res = generate_restaurant_and_items(cuisine)
+    st.header(res['restaurant_name'])
+    st.write("**Menu Items**")
+    items = res['menu_items'].split(",")
+    for i in items:
+        st.write("-", i)
